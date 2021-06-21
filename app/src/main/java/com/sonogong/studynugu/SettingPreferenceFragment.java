9ab4f34f8c02e36f6e5373b4d9c45e8305fe7dd4
s2019s35 <s2019s35@e-mirim.hs.kr>
2021-06-21 16:01:12 +0900

    asmr service update

--- app/src/main/java/com/sonogong/studynugu/SettingPreferenceFragment.java
+++ app/src/main/java/com/sonogong/studynugu/SettingPreferenceFragment.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -33,11 +34,26 @@ public class SettingPreferenceFragment extends PreferenceFragment {
     DdayDAO ddayDAO;
     TodoDAO todoDAO;
     StopwatchDAO stopwatchDAO;
+    private ListPreference mASMRLIST;
+    MediaPlayer mediaPlayer = null;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.setting);
+
+        mASMRLIST = (ListPreference)getPreferenceScreen().findPreference("asmr_list");
+        mASMRLIST.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                String value = (String) newValue;
+                if(preference == mASMRLIST){
+                    if(mediaPlayer != null)mediaPlayer.stop();
+                    music(value);
+                }
+                return true;
+            }
+        });
 
     }
 
@@ -59,6 +75,41 @@ public class SettingPreferenceFragment extends PreferenceFragment {
             }
         }
 
+    }
+    void music(String title){
+        if(title.equals("주륵주륵 비")){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.rain);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.start();
+        }else if(title.equals("철썩철썩 파도")){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.wave);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.start();
+        }else if(title.equals("휘잉휘잉 바람")){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.wind);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.start();
+        }else if(title.equals("찌르르르 풀벌레")){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.bug);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.start();
+        }else if(title.equals("타닥타닥 키보드")){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.keyboard);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.start();
+        }else if(title.equals("사각사각 연필")){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.pencil);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.start();
+        }else if(title.equals("위잉위잉 비행기")){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.airplane);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.start();
+        }else if(title.equals("열공열공 도서관")){
+            mediaPlayer = MediaPlayer.create(getActivity(), R.raw.rain);
+            mediaPlayer.setLooping(true);
+            mediaPlayer.start();
+        }
     }
 
     void show()
